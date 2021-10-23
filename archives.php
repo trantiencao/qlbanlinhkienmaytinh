fa0fedfb29ad3262dc5481107acf03b7c35046f7
Trần Tiến Cao - 59130151 <cao.tt.59cntt@ntu.edu.vn>
2021-10-24 01:06:43 +0700

    1.0

--- archives.php
+++ archives.php
@@ -1,7 +1,7 @@
 <?php require("layouts/header.php"); ?>
 <div class="main">
 	<div class="main-top">
-		<div class="section group">
+		<div class="section group"> 
 			<div class="col_1_of_3 span_1_of_3">
 				<h3>Lorem Ipsum is simply</h3>
 				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
